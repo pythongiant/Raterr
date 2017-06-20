103068d57e6cf85522befe69574c816dfd129023
pythongiant <srihari.unnikrishnan@gmail.com>
2017-06-20 11:54:59 +0530

    init

---
+++ windows.py
@@ -0,0 +1,16 @@
+#Import Libraries and create a makeshift loading screen
+import os
+print("please wait For a few moments")
+from textblob import TextBlob
+
+#clear terminal of loading screen
+os.system('cls')
+
+#User Input for rating
+Rating=raw_input("what is your rating:")
+#convert to TextBlob String
+rating= TextBlob(Rating)
+#covert out of 100 to out of 5
+User_rating= rating.sentiment.polarity*5
+#print out rating
+print ("expected User rating:"+str(User_rating)+"/5")
